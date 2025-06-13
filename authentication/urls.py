c71ccdf6fb58036908e2701992ecde59c2d61f62
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-06-13 19:50:07 +0530

    ..

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ router = DefaultRouter()
 router.register(r'staff', StaffAccountViewSet)
 
 urlpatterns = [
-    path('staff/', include(router.urls)),
+    path('staffs/', include(router.urls)),
     
 ]
 # http://127.0.0.1:8000/auth/staff/staff/1/reset_password/ to reset password
